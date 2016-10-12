f317c0c2926e8f9a50fc750a2cc2322ee7163b74
Daniel Beardsley <daniel@ifixit.com>
2016-10-12 00:56:19 -0700

    operations: make more flexible
    Now an operation can do anything to the S3 object. An easy to reuse
    modify-headers.js is provided.

--- operations/fix-cache-headers.js
+++ operations/fix-cache-headers.js
@@ -1,6 +1,8 @@
-module.exports = function fixCacheHeaders(headers) {
+var modifyHeaders = require('../modify-headers.js');
+
+module.exports = modifyHeaders(function fixCacheHeaders(headers) {
    delete headers.Expires;
    headers.CacheControl = 'max-age=31557600';
    return headers;
-}
+});
 
