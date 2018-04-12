500de93d631b71800bdbce9088c0acfdb18ccd8f
Daniel Beardsley <daniel@ifixit.com>
2018-04-12 00:39:28 -0700

    list-buckets: add --all-buckets option
    For when you want to go big or go home!

--- list-objects.js
+++ list-objects.js
@@ -3,11 +3,12 @@ var s3 = new AWS.S3({apiVersion: '2006-03-01'});
 var argv = require('yargs').argv;
 var completed = 0;
 
-if (!argv.buckets) {
+if (!argv.buckets && !argv["all-buckets"]) {
    console.error([
-    "Usage: node list-objects.js --buckets=bucket1,bucket2 > all-objects.list",
+    "Usage: node list-objects.js (--buckets=bucket1,bucket2|--all-buckets) > all-objects.list",
     "   --buckets : comma-delimited list of s3 bucket names to list the ",
     "               contents of.",
+    "   --all-buckets : lists all objects in all buckets",
     "",
     "Lists the contents of s3 buckets in a format compatable ",
     "with head-blaster.js.",
@@ -16,14 +17,15 @@ if (!argv.buckets) {
    process.exit(-1);
 }
 
-var buckets = argv.buckets.split(",")
-buckets.forEach(function(bucketName) {
+forEachBucket(function(bucketName, next) {
+   log("\rListing from bucket: " + bucketName + "\n");
    s3.listObjects({Bucket:bucketName}).eachPage(
    function(err, data) {
       if (err) {
          console.error(err);
          process.exit(1);
       }
+      if (!data) return next();
       setTimeout(function() {
          data.Contents.forEach(function(object) {
             process.stdout.write(bucketName + ":" + object.Key + "\n");
@@ -33,6 +35,35 @@ buckets.forEach(function(bucketName) {
       }, 0);
    });
 });
+
+function forEachBucket(callback) {
+   var buckets = [];
+   if (argv.buckets) {
+      buckets = argv.buckets.split(",");
+      popBucket();
+   } else {
+      s3.listBuckets().eachPage(
+      function(err, data) {
+         if (err) {
+            console.error(err);
+            process.exit(1);
+         }
+         if (data) {
+            newBuckets = data.Buckets.map((bucket) => bucket.Name);
+            buckets = buckets.concat(newBuckets);
+            log("Got " + data.Buckets.length + " buckets\n");
+         } else {
+            popBucket();
+         }
+      });
+   }
+
+   function popBucket() {
+      var bucket = buckets.shift();
+      if (!bucket) return;
+      callback(bucket, popBucket);
+   }
+}
 
 function log(str) {
    process.stderr.write(str);
